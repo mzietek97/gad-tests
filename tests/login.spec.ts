e6f65821b94218e9510e1e8c2d435da4cd952489
Michał Ziętek <michal.zietek.997@gmail.com>
2023-11-19 18:21:41 +0100

    chore: Test data for user mail and password

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,13 @@
 import { LoginPage } from '../src/pages/login.page';
 import { WelcomePage } from '../src/pages/welcome.page';
+import { testUser1 } from '../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
   test('login with correct credentials @GAD_R02_01', async ({ page }) => {
     // Arrange
-    const userEmail = 'Moses.Armstrong@Feest.ca';
-    const userPassword = 'test1';
+    const userEmail = testUser1.userEmail;
+    const userPassword = testUser1.userPassword;
     const loginPage = new LoginPage(page);
 
     // Act
