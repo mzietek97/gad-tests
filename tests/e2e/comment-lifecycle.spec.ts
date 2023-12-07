dde2750c46406181eee68924a9f5c450b8687230
Michał Ziętek <michal.zietek.997@gmail.com>
2023-12-07 21:18:39 +0100

    chore: refactor comment views page return PO

--- tests/e2e/comment-lifecycle.spec.ts
+++ tests/e2e/comment-lifecycle.spec.ts
@@ -36,7 +36,7 @@ test.describe('Create, verify and delete comment', () => {
       await expect
         .soft(addCommentView.addNewHeader)
         .toHaveText(expectedAddCommentHeader);
-      await addCommentView.createComment(newCommentData);
+      articlePage = await addCommentView.createComment(newCommentData);
 
       // Assert
       await expect
@@ -44,7 +44,7 @@ test.describe('Create, verify and delete comment', () => {
         .toHaveText(expectedCommentCreatedPopup);
     });
 
-    const commentPage = await test.step('verify comment', async () => {
+    let commentPage = await test.step('verify comment', async () => {
       // Act
       const articleComment = articlePage.getArticleComment(newCommentData.body);
       await expect(articleComment.body).toHaveText(newCommentData.body);
@@ -66,7 +66,7 @@ test.describe('Create, verify and delete comment', () => {
 
       // Act
       const editCommentView = await commentPage.clickEditButton();
-      await editCommentView.updateComment(editCommentData);
+      commentPage = await editCommentView.updateComment(editCommentData);
 
       // Assert
       await expect
@@ -94,7 +94,7 @@ test.describe('Create, verify and delete comment', () => {
 
       // Act
       const addCommentView = await articlePage.clickAddCommentButton();
-      await addCommentView.createComment(newCommentData);
+      articlePage = await addCommentView.createComment(newCommentData);
 
       // Assert
       await expect
@@ -106,7 +106,7 @@ test.describe('Create, verify and delete comment', () => {
       const secondCommentBody = await test.step('create comment', async () => {
         const secondCommentData = prepareRandomComment();
         const addCommentView = await articlePage.clickAddCommentButton();
-        await addCommentView.createComment(secondCommentData);
+        articlePage = await addCommentView.createComment(secondCommentData);
         return secondCommentData.body;
       });
 
