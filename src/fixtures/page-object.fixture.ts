24da968c184d3bc584595656c4a4248d441f08ed
Michał Ziętek <michal.zietek.997@gmail.com>
2023-12-17 16:06:17 +0100

    chore: own fixtures in smoke tests

--- src/fixtures/page-object.fixture.ts
+++ src/fixtures/page-object.fixture.ts
@@ -1,10 +1,12 @@
 import { ArticlesPage } from '@_src/pages/articles.page';
 import { CommentsPage } from '@_src/pages/comments.page';
+import { HomePage } from '@_src/pages/home.page';
 import { test as baseTest } from '@playwright/test';
 
 interface Pages {
   articlesPage: ArticlesPage;
   commentsPage: CommentsPage;
+  homePage: HomePage;
 }
 
 export const pageObjectTest = baseTest.extend<Pages>({
@@ -18,5 +20,11 @@ export const pageObjectTest = baseTest.extend<Pages>({
     const commentsPage = new CommentsPage(page);
     await commentsPage.goto();
     await use(commentsPage);
+  },
+
+  homePage: async ({ page }, use) => {
+    const homePage = new HomePage(page);
+    await homePage.goto();
+    await use(homePage);
   },
 });
