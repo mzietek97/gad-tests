26cd505618be56fafad75a278f04a4f24ef34152
Michał Ziętek <michal.zietek.997@gmail.com>
2024-01-30 21:05:27 +0100

    test: Go button action

---
+++ tests/integration/search.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+test.describe('Verify search component for articles', () => {
+    test('Go button should fetch articles @GAD-R07-01', async ({
+        articlesPage,
+        page,
+    }) => {
+        // Arrange
+        await expect(articlesPage.goSearchButton).toBeInViewport();
+        const responsePromise = page.waitForResponse('/api/articles*');
+        const expectDefaultArticleNumber = 6;
+
+        // Act
+        await articlesPage.goSearchButton.click();
+        const response = await responsePromise;
+        const body = await response.json();
+
+        // Assert
+        expect(response.ok()).toBeTruthy();
+        expect(body).toHaveLength(expectDefaultArticleNumber);
+    });
+});
