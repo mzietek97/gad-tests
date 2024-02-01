4f423ab9e27b42280c7897f019410eb53d247e78
Michał Ziętek <michal.zietek.997@gmail.com>
2024-02-01 15:44:48 +0100

    test: api verification after article creation

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,3 +1,4 @@
+import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
 import { waitForResponse } from '@_src/utils/wait.utils';
@@ -94,5 +95,32 @@ test.describe('Verify articles', () => {
                 .toHaveText(articleData.title);
             expect(response.status()).toBe(expectedResponseCode);
         });
+    });
+
+    test('should return created article from API @GAD-R07-04 @logged', async ({
+        addArticleView,
+        page,
+    }) => {
+        // Arrange
+        const articleData = prepareRandomArticle();
+        const responsePromise = page.waitForResponse(
+            (response) => {
+                return (
+                    response.url().includes('/api/articles') &&
+                    response.request().method() == 'GET'
+                );
+            },
+            { timeout: RESPONSE_TIMEOUT },
+        );
+
+        // Act
+        const articlePage = await addArticleView.createArticle(articleData);
+        const response = await responsePromise;
+
+        // Assert
+        await expect
+            .soft(articlePage.articleTitle)
+            .toHaveText(articleData.title);
+        expect(response.ok).toBeTruthy();
     });
 });
