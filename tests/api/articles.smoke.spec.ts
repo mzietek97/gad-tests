8d5e6b6de4176a42ebd453d4da937d8ce8d21ea1
Michał Ziętek <michal.zietek.997@gmail.com>
2024-03-12 21:01:12 +0100

    chore: tag added for api test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -1,7 +1,9 @@
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify articles API endpoints', () => {
-    test('GET articles returns status code 200', async ({ request }) => {
+    test('GET articles returns status code 200 @GAD-R08-01 @api', async ({
+        request,
+    }) => {
         // Arrange
         const expectedStatusCode = 200;
         const articlesUrl = '/api/articles';
