af98f89659a910585c348995772fcf06bbafe5df
Michał Ziętek <michal.zietek.997@gmail.com>
2024-03-23 19:38:18 +0100

    test: api smoke articles extended test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -1,9 +1,7 @@
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify articles API endpoints', () => {
-    test('GET articles returns status code 200 @GAD-R08-01 @api', async ({
-        request,
-    }) => {
+    test('GET articles returns status code 200 @GAD-R08-01 @api', async ({ request }) => {
         // Arrange
         const expectedStatusCode = 200;
         const articlesUrl = '/api/articles';
@@ -13,5 +11,18 @@ test.describe('Verify articles API endpoints', () => {
 
         // Assert
         expect(response.status()).toBe(expectedStatusCode);
+    });
+
+    test('GET articles should return at least one article', async ({ request }) => {
+        // Arrange
+        const expectedMinArticleCount = 1;
+        const articlesUrl = '/api/articles';
+
+        // Act
+        const response = await request.get(articlesUrl);
+        const responseJson = await response.json();
+
+        // Assert
+        expect(responseJson.length).toBeGreaterThanOrEqual(expectedMinArticleCount);
     });
 });
